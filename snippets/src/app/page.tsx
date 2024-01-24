f6d93e984c4067b70ce2b59e2d400e9431b875f1
OlegsIljanovs <42992921+CoconutTree01@users.noreply.github.com>
2024-01-24 19:34:44 +0200

    Display snippets in homepage

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>Home Page</div>
+import { db } from '@/db'
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+  
+  const renderSnippets = snippets.map((snippet) => {
+    return (
+      <div key={snippet.id}>
+        {snippet.title}
+      </div>
+    )
+  })
+
+  return <div>{renderSnippets}</div>
 }
