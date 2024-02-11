18c1af47e4b78a4a887190d1f70305147cbefe7f
OlegsIljanovs <42992921+CoconutTree01@users.noreply.github.com>
2024-02-11 20:35:55 +0200

    Error handling  added

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -20,22 +20,43 @@ export async function deleteSnippet (id: number) {
     redirect('/');
 }
 
-export async function createSnippet(formState: {message: string}, formData: FormData){
-    return {
-        message: 'Title must be longer'
-    }
-    // // Check the user's inputs and make sure they're valid
-    // const title = formData.get('title') as string;
-    // const code = formData.get('code') as string;
+export async function createSnippet(
+    formState: {message: string}, formData: FormData
+    ){
+    // Check the user's inputs and make sure they're valid
+    try {
+        const title = formData.get('title');
+        const code = formData.get('code');
 
-    // // Create a new record in the database
-    // const snippet = await db.snippet.create({
-    //     data: {
-    //         title: title,
-    //         code: code
-    //     }
-    // });
-    // console.log(snippet);
-    // // Redirect the user back to the root route
-    // redirect('/')
+        if (typeof title !== 'string' || title.length < 3) {
+            return {
+                message: 'Title must be longer',
+        }
+        };
+        if (typeof code !== 'string' || code.length < 10) {
+            return {
+                message: 'Code must be longer',
+            }
+        }
+        // Create a new record in the database
+        await db.snippet.create({
+            data: {
+                title: title,
+                code: code
+            }
+        });
+    } catch (err: unknown) {
+        if (err instanceof Error){
+            return {
+                message: err.message
+            };
+
+        } else {
+            return {
+                message: 'Something went wrong.'
+            }
+        }
+    }
+    // Redirect the user back to the root route
+    redirect('/')
 }
